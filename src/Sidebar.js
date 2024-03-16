168b1d99ec5e9e301961991b50c33153358674d9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:42:26 +0430

    init

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
   return <h2>sidebar component</h2>
