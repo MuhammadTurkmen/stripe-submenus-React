6023a6703ee110cba863aab679fbacaae41cbd2e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:09:38 +0430

    init

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { AppProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
