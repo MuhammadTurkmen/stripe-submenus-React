50c34ea4d5e59fd04dabf66e5f79daecb17d2bd7
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:13:54 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,6 +5,7 @@ import { useGlobalContext } from './context'
 
 
 const Navbar = () => {
+  const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext()
   return <h2>navbar component</h2>
 }
 
