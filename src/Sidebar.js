1b5cfe327782c03cea760faa4de0d41e7bc79c4d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:43:28 +0430

    init

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,7 @@ import sublinks from './data'
 import { useGlobalContext } from './context'
 
 const Sidebar = () => {
+  const {isSidebarOpen, closeSidebar} = useGlobalContext()
   return <h2>sidebar component</h2>
 }
 
