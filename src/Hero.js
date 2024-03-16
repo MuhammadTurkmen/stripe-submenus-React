3a776ddae7c2b18071c235df96500815208b5976
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:39:07 +0430

    init

--- src/Hero.js
+++ src/Hero.js
@@ -8,6 +8,10 @@ const Hero = () => {
     <div className="hero-center">
       <article className='hero-info'>
         <h1>Payments infrastructure for the internet</h1>
+        <p>Millions of companies of all sizes-from startups tp Forturne 500s-use
+          Stripe's software and APIs to accept payments, send payouts, and
+          manage their businesses online
+        </p>
       </article>
       <article className='hero-images'>
         <img src={phoneImg} className='phone-img' alt="phone" />
