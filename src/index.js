b039263459942083ee1421fa54ce3e19d880d006
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:09:10 +0430

    init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AppProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
