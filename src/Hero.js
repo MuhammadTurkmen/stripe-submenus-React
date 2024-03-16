068f2bea9a69c8c5d5f4fc36aa462a740d1f6c1b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:12:38 +0430

    init

--- src/Hero.js
+++ src/Hero.js
@@ -3,6 +3,8 @@ import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
+  const data = useGlobalContext()
+  console.log(data);
   return <h2>hero component</h2>
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
 
 const Navbar = () => {
   return <h2>navbar component</h2>
