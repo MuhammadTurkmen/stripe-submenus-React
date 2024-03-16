bcb4d1d659f79b4f181421db55b10ebedf8570b5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:01:16 +0430

    init

--- src/context.js
+++ src/context.js
@@ -6,5 +6,13 @@ const AppContext = React.createContext()
 const AppProvider = ({children}) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true)
     const [isModalOpen, setIsModalOpen] = useState(true)
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+    
     return <AppContext.Provider>{children}</AppContext.Provider>
 }
