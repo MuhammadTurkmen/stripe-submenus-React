cdda9b3953600d73a18932353cae5544be41b488
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:18:10 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,7 +10,9 @@ const Navbar = () => {
     <div className="navcenter">
       <div className="nav-header">
         <img src={logo} className='nav-logo' alt="stripe" />
-        <button className='btn toggle-btn' onClick={openSidebar}></button>
+        <button className='btn toggle-btn' onClick={openSidebar}>
+          <FaBars />
+        </button>
       </div>
       <ul className='nav-links'>
 
