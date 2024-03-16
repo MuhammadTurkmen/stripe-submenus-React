cdec34d94963886bb341541f73c4b165db195be2
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 10:36:04 +0430

    init

--- src/Hero.js
+++ src/Hero.js
@@ -6,7 +6,9 @@ const Hero = () => {
   const {closeSubmenu} = useGlobalContext()
   return <section className='hero'>
     <div className="hero-center">
-      <article></article>
+      <article className='hero-info'>
+        
+      </article>
       <article className='hero-images'>
         <img src={phoneImg} className='phone-img' alt="phone" />
       </article>
