03ac81e6225f16e6f7046351c059d4d5ca3f0c34
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-16 09:59:29 +0430

    init

--- src/context.js
+++ src/context.js
@@ -4,5 +4,7 @@ import sublinks from './data'
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true)
     return <AppContext.Provider>{children}</AppContext.Provider>
 }
