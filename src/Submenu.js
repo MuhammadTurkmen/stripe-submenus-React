7a44e793074ca788383e0e1895046667ad5a8ca9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-17 19:09:13 +0430

    init

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,13 +2,17 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
+
   const {isSubmenuOpen, location, page:{page, links}} = useGlobalContext()
+  
   const container = useRef(null)
+  const [columns, setColumns] = useState('col-2')
   useEffect(() => {
     const submenu = container.current
     const {center , bottom} = location
     submenu.style.left = `${center}px`
     submenu.style.top = `${bottom}px`
+    
   }, [location])
   return <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`} ref={container}>
     <h4>{page}</h4>
