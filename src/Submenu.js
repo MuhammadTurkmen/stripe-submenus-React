afc7a4672ddc8d759f047f48294406c5c00d57ea
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-17 18:44:59 +0430

    init

--- src/Submenu.js
+++ src/Submenu.js
@@ -3,6 +3,9 @@ import { useGlobalContext } from './context'
 
 const Submenu = () => {
   const {isSubmenuOpen, location} = useGlobalContext()
+  useEffect(() => {
+
+  }, [location])
   return <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>submenu</aside>
 }
 
