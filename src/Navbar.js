82eded85bdb035b07905728b4bf9b8f9484e8e36
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-17 18:38:39 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     const page = e.target.textContent
     const tempBtn = e.target.getBoundingClientRect()
     const center = (tempBtn.left + tempBtn.right) / 2
-    const bottom = tempBtn.bottom
+    const bottom = tempBtn.bottom - 3
     openSubmenu()
   }
   
