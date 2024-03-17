c5b565599fb32f019c33ebe5ad4e3dce37e242dd
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-17 18:42:19 +0430

    init

--- src/context.js
+++ src/context.js
@@ -18,6 +18,7 @@ export const AppProvider = ({children}) => {
     
 
     const openSubmenu = (text, coordinates) => {
+        setLocation(coordinates)
         setIsSubmenuOpen(true)
     }
     const closeSubmenu = () => {
