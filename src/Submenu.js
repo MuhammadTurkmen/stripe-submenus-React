001051d9ac42f9dc85a6df41b76eaf661679561d
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-17 18:11:51 +0430

    init

--- src/Submenu.js
+++ src/Submenu.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useGlobalContext } from './context'
 
 const Submenu = () => {
+  const {isSubmenuOpen} = useGlobalContext()
   return <h2>submenu component</h2>
 }
 
